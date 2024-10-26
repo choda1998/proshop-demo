bc48da071ac4974bfcd8bf825755986c8bcd767c
Danilo Vujakovic <vujakovicdanilo@gmail.com>
2024-10-26 23:43:58 +0200

    Frontend and backend init, fetching without db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
-const s = () => {
+const App = () => {
   return (
-    <h1>Welcome To ProShop</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default s
+export default App;
